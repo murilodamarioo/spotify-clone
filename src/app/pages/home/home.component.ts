b1c62981631cd7b8b42f723d1ee3af9bdfb7dac1
Murilo Damario <murilo.gcdamario@gmail.com>
2022-12-20 18:36:56 -0300

    Implementa tabela de exibição das músicas

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faClock, faPlay } from '@fortawesome/free-solid-svg-icons';
 import { IMusic } from 'src/app/interfaces/IMusic';
 import { SpotifyService } from 'src/app/services/spotify.service';
 
@@ -11,6 +12,9 @@ export class HomeComponent implements OnInit {
 
   musics: IMusic[] = []
 
+  playIcon = faPlay
+  timerIcon = faClock
+
   constructor(private spotifyService: SpotifyService) {}
 
   ngOnInit() {
@@ -20,6 +24,10 @@ export class HomeComponent implements OnInit {
   async getMusics() {
      this.musics = await this.spotifyService.searchMusics()
      console.log(this.musics)
+  }
+
+  getArtists(music: IMusic) {
+    return music.artists.map(artist => artist.name).join(', ')
   }
 
 }
